c7e624e4f3f501350c94ea262b71faefa684cb08
rotwang00 <paulthurmond@gmail.com>
2022-08-02 10:14:43 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const add = function (x, y) {
+  return x + y;
+};
+
+const subtract = function (x, y) {
+  return x - y;
+};
+
+const multiply = function (x, y) {
+  return x * y;
+};
+
+const divide = function (x, y) {
+  return x / y;
+};
+
+const operate = function (operator, x, y) {};
