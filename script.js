e25d82725485f3634fd57e95371f5aa7ff870790
rotwang00 <paulthurmond@gmail.com>
2022-08-03 08:15:26 -0500

    Create basic css

--- script.js
+++ script.js
@@ -14,4 +14,21 @@ const divide = function (x, y) {
   return x / y;
 };
 
-const operate = function (operator, x, y) {};
+const operate = function (operator, x, y) {
+  switch (operator) {
+    case '+':
+      return add(x, y);
+      break;
+    case '-':
+      return subtract(x, y);
+      break;
+    case '*':
+      return multiply(x, y);
+      break;
+    case '/':
+      return divide(x, y);
+      break;
+  }
+};
+
+console.log(operate('/', 5, 6));
